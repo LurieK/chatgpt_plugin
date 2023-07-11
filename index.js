6329a6e0774dc879be95ede42572161d999078d4
LurieK <LuraineKimmerle@gmail.com>
2023-07-11 17:31:44 +0800

    write test object for function

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
+let books = [
+    { genre: 'Fantasy', title: 'A Game of Thrones', author: 'George R. R. Martin'},
+    { genre: 'Mystery', title: 'The Da Vinci Code', author: 'Dan Brown' },
+    { genre: 'Thriller', title: 'The Girl with the Dragon Tattoo', author: 'Stieg Larsson' },
+    { genre: 'Science Fiction', title: 'Dune', author: 'Frank Herbert' }
+]
