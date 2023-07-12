af06ab4b9da792f492b4e28fd1e3fe84875f16de
LurieK <LuraineKimmerle@gmail.com>
2023-07-12 13:59:49 +0800

    define variables, import and port

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-const express = require('express');
-const router = express.Router();
+const express = requre('express');
+const cors = require('cors');
+const recRouter = require('./routes/recommend');
+const openaiRoutes= require('./routes/openai')
 
-let books = [
-    { genre: 'Fantasy', title: 'A Game of Thrones', author: 'George R. R. Martin'},
-    { genre: 'Mystery', title: 'The Da Vinci Code', author: 'Dan Brown' },
-    { genre: 'Thriller', title: 'The Girl with the Dragon Tattoo', author: 'Stieg Larsson' },
-    { genre: 'Science Fiction', title: 'Dune', author: 'Frank Herbert' }
-];
-
-const getBookRecommendaton= async function(req, res) {
-    const {genre} = req.body;
-    const book = books.find(book.genre === genre);
-    if (book){
-        res.json(book);
-    }else{
-        res.status(404).json({error: 'No books found for that genre'})
-    }
-}
-
-router.post('/', getBookRecommendaton);
-
-module.exports = router
+const app = express();
+const PORT = 3000;
---
+++ routes/recommend.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+let books = [
+    { genre: 'Fantasy', title: 'A Game of Thrones', author: 'George R. R. Martin'},
+    { genre: 'Mystery', title: 'The Da Vinci Code', author: 'Dan Brown' },
+    { genre: 'Thriller', title: 'The Girl with the Dragon Tattoo', author: 'Stieg Larsson' },
+    { genre: 'Science Fiction', title: 'Dune', author: 'Frank Herbert' }
+];
+
+const getBookRecommendaton= async function(req, res) {
+    const {genre} = req.body;
+    const book = books.find(book.genre === genre);
+    if (book){
+        res.json(book);
+    }else{
+        res.status(404).json({error: 'No books found for that genre'})
+    }
+}
+
+router.post('/', getBookRecommendaton);
+
+module.exports = router
