ff0bdd806335c23c63dd6b795cc923a251ddc541
LurieK <LuraineKimmerle@gmail.com>
2023-07-27 13:43:43 +0800

    fix spelling error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = requre('express');
+const express = require('express');
 const cors = require('cors');
 const recRouter = require('./routes/recommend');
 const openaiRoutes= require('./routes/openai')
