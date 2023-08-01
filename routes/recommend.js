022f006b92e4f808c749a0d669e889badb357462
LurieK <LuraineKimmerle@gmail.com>
2023-08-01 12:07:45 +0800

    import books.js

--- routes/recommend.js
+++ routes/recommend.js
@@ -1,27 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { getGenre, getBooksByGenre, getBooksByAuthor} = require ('./books')
 
-let books = [
-    { genre: 'Fantasy', title: 'A Game of Thrones', author: 'George R. R. Martin'},
-    { genre: 'Mystery', title: 'The Da Vinci Code', author: 'Dan Brown' },
-    { genre: 'Thriller', title: 'The Girl with the Dragon Tattoo', author: 'Stieg Larsson' },
-    { genre: 'Science Fiction', title: 'Dune', author: 'Frank Herbert' }
-];
 
-const getBookRecommendaton= async function(req, res) {
-    const {genre} = req.body;
-
-    if (!genre){
-        return res.status(400).json({error:'Genre is required'})
-    }
-    const book = books.find(book=> book.genre === genre);
-    if (book){
-        res.json(book);
-    }else{
-        res.status(404).json({error: 'No books found for that genre'})
-    }
-}
-
-router.post('/', getBookRecommendaton);
 
 module.exports = router
