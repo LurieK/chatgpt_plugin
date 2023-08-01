3f38c2ec4af781c95cc7017999acd3f48978413b
LurieK <LuraineKimmerle@gmail.com>
2023-08-01 12:31:56 +0800

    post request- getBooksByAuthor & getBooksByGenre

--- routes/recommend.js
+++ routes/recommend.js
@@ -6,6 +6,20 @@ router.post('/getGenre', async (req, res)=> {
     const title= req.body.title;
     const result = await getGenre(title);
     res.json(result);
+});
+
+router.post('/getBooksByAuthor', async(req, res)=>{
+    const author = req.body.author;
+    const result = await getBooksByAuthor(author);
+    res.json(result);
 })
+
+router.post('/getBooksByGenre', async (req, res)=> {
+    const genres = req.body.genres;
+    const result = await getBooksByGenre(genres);
+    res.json.apply(result);
+})
+
+
 
 module.exports = router
