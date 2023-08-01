b98885d8ffa01493c205dd1abd3d009689934e7f
LurieK <LuraineKimmerle@gmail.com>
2023-08-01 12:14:47 +0800

    post rout for getGenre

--- routes/recommend.js
+++ routes/recommend.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { getGenre, getBooksByGenre, getBooksByAuthor} = require ('./books')
 
-
+router.post('/getGenre', async (req, res)=> {
+    const title= req.body.title;
+    const result = await getGenre(title);
+    res.json(result);
+})
 
 module.exports = router
